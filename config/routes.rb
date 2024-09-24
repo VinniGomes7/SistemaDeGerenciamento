5de3780159b9f8e361ad91907cb818b876ea558c
VinniGomes7 <141679025+VinniGomes7@users.noreply.github.com>
2024-09-24 09:32:48 -0300

    Atualiza configuração do GitHub Actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :candidatos
   resources :candidaturas
   resources :vagas
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   resources :empregadores
   
   # Rotas para login e logout
+  root "home#index"
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 end
