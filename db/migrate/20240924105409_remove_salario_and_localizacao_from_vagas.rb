5d212f55bc8e9cdbf9ff1d99d53dcc09851b0a2e
VinniGomes7 <141679025+VinniGomes7@users.noreply.github.com>
2024-09-24 10:54:51 -0300

    Correção schema.rb

--- db/migrate/20240924105409_remove_salario_and_localizacao_from_vagas.rb
+++
@@ -1,6 +0,0 @@
-class RemoveSalarioAndLocalizacaoFromVagas < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :vagas, :salario, :decimal
-    remove_column :vagas, :localizacao, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_24_105409) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_24_065039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,9 +34,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_24_105409) do
   create_table "empregadors", force: :cascade do |t|
     t.string "nome", null: false
     t.string "email", null: false
+    t.string "telefone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "telefone"
   end
 
   create_table "usuarios", force: :cascade do |t|
